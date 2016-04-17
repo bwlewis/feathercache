76c1f602391e25f99b0499c5b8dc293915ed7b8b
bwlewis <blewis@illposed.net>
2016-04-17 18:29:46 -0400

    Added htdigest function
    for maintaining digest authentication files

--- R/util.R
+++ R/util.R
@@ -4,3 +4,29 @@ urlEncodePath = function (x)
   enc = utils::URLencode(x, reserved=TRUE)
   gsub("%2[Ff]", "/", x)
 }
+
+#' Create or Update a Digest Authentication File
+#' Use this function to create or edit 'htdigest'-style HTTP digest authentication
+#' files for the \code{\link{mongoose}} back end. Depending on the authentication
+#' settings (global or per-directory), you may use just one global file or a file
+#' in each subdirectory of the mongoose data directory.
+#' @param file An 'htdigest'-style file to create or update.
+#' @param realm The authentication realm.
+#' @param user User name.
+#' @param password User password.
+#' @return Invoked for the side effect of creating or updating \code{file}.
+#' @export
+htdigest = function(file, realm, user, password)
+{
+  entry = sprintf("%s:%s:%s", user, realm,
+            digest::digest(charToRaw(sprintf("%s:%s:%s", user, realm, password)), serialize=FALSE, algo="md5"))
+  f = tryCatch(readLines(file), error=function(e) c())
+  key = sprintf("%s:%s:", user, realm)
+  idx = grep(key, f)
+  if(length(idx) > 0)
+  {
+    f[idx[1]] = entry
+  }
+  else f = c(f, entry)
+  writeLines(f, con=file)
+}
