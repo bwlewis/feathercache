141794c89cf6f10b56af9a25b81d251ed9e925c1
bwlewis <blewis@illposed.net>
2016-04-19 12:40:09 -0400

    linting, travis

--- R/util.R
+++ R/util.R
@@ -1,5 +1,5 @@
 # adapted from the htmltools package; we only need this one function
-urlEncodePath = function (x) 
+urlEncodePath = function (x)
 {
   enc = utils::URLencode(x, reserved=TRUE)
   gsub("%2[Ff]", "/", enc)
