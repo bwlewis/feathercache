dca89ee4a5dfbe80ba5481d002f3fbe458ec3820
bwlewis <blewis@illposed.net>
2016-04-19 23:39:26 -0400

    tests
    htdigest test

---
+++ tests/util.R
@@ -0,0 +1,10 @@
+check = function(a, b)
+{
+  print(match.call())
+  stopifnot(all.equal(a, b, check.attributes=FALSE, check.names=FALSE))
+}
+
+library("feathercache")
+file = tempfile()
+htdigest(file, "realm", "user", "password")
+check("user:realm:ebbc0ff9a121dbb6789bbe5f82174fa0", system(paste("cat", file), intern=TRUE))
